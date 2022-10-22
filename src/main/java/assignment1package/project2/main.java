a462a2a5bdf43a3b127fa72fcbad69e2d19bb1ee
ewisniew0 <e.wisniewski1@nuigalway.ie>
2022-10-22 17:11:20 +0100

    fixes main class object initialization and adds printing

--- src/main/java/assignment1package/project2/main.java
+++ src/main/java/assignment1package/project2/main.java
@@ -1,29 +1,66 @@
 package assignment1package.project2;
 import assignment1package.*;
+import org.joda.time.DateTime;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class main {
-    student[] student_array = {
-            new student("John", 21, LocalDate.of(2000, 12, 31), 111111111, "BCT", new String[]{"CT100", "CT200"}),
-            new student("James", 22, LocalDate.of(1999, 11, 30), 111111112, "BCT", new String[]{"CT100", "CT200"}),
-            new student("Jack", 21, LocalDate.of(2000, 11, 29), 111111113, "BCT", new String[]{"CT100", "CT200"})
-    };
 
-    lecturer lecturer = new lecturer("Jones", 41, LocalDate.of(1980, 12, 31), 222222222, new String[]{"CT100", "CT200"});
+    public static void main(String[] args) {
 
-    module[] module_array = {
-            new module("Software Engineering", 100, student_array, LocalDate.of(2000, 12, 31), LocalDate.of(2000, 12, 31), lecturer.getModules_teaching(), lecturer),
-            new module("Cryptography", 200, student_array, LocalDate.of(2000, 12, 31), LocalDate.of(2000, 12, 31), lecturer.getModules_teaching(), lecturer),
-    };
+        course_program course = new course_program("Computer Science & IT", new DateTime(2020, 8, 1, 8, 0), new DateTime(2020, 8, 1, 8, 0));
+        course_program course2 = new course_program("Mathematics", new DateTime(2020, 8, 1, 8, 0), new DateTime(2020, 8, 1, 8, 0));
 
-    course_program course = new course_program("Computer Science & IT", module_array, student_array, new org.joda.time.DateTime());
+        module[] module_array = {
+                new module("Software Engineering", 101, new DateTime(2020, 8, 1, 8, 0), new DateTime(2020, 8, 1, 8, 0)),
+                new module("Cryptography", 202, new DateTime(2020, 8, 1, 8, 0), new DateTime(2020, 8, 1, 8, 0))
+        };
 
 
+        student[] student_array = {
+                new student("John", 21, LocalDate.of(2000, 12, 31), 111111111, course, module_array),
+                new student("James", 22, LocalDate.of(1999, 11, 30), 111111112, course, module_array),
+                new student("Jack", 21, LocalDate.of(2000, 11, 29), 111111113, course, module_array)
+        };
+
+        lecturer lecturer = new lecturer("Jones", 41, LocalDate.of(1980, 12, 31), 222222222, module_array);
+
+        course.setModule_list(module_array);
+        course2.setModule_list(module_array);
+
+        course.setEnrolled_students(student_array);
+        course2.setEnrolled_students(student_array);
+
+        for (int i = 0; i < module_array.length; i++) {
+           module_array[i].setEnrolled_students(student_array);
+           module_array[i].setLecturer_responsible(lecturer);
+        }
+
+        List<course_program> course_list = new ArrayList<course_program>();
+        course_list.add(course);
+        course_list.add(course2);
 
 
+        for (int i = 0; i < course_list.size(); i++) {
+            System.out.println("\nCourses List:\n");
+            System.out.println("\t"+course_list.get(i).getCourse_name() + "\n");
 
+            System.out.println("\t\tModule List:\n");
+            for (int j = 0; j < module_array.length; j++) {
+                System.out.println("\t\t"+module_array[j].getModule_name());
+            }
 
+            System.out.println("\n\t\t\tStudent List:\n");
+            for (int j = 0; j < student_array.length; j++) {
+                System.out.println("\t\t\t"+student_array[j].getName());
+            }
+
+        }
+    }
 }
+
+
 
---
+++ src/test/java/assignment1package/project2/mainTest.java
@@ -0,0 +1,17 @@
+package assignment1package.project2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
